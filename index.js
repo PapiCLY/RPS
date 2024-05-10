187e07447819a73f43fbbef1e550fe55fd563de6
Yehudah Christian <121639934+PapiCLY@users.noreply.github.com>
2024-05-09 21:42:24 -0400

    slight bug, fixed, and game logic works, need to create reset button and loading on each rps game until user gets to 10

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const imageButtons = document.querySelectorAll('.imgBtn')
+let playerScore = 0
+let cpuScore = 0
+let ties = 0
 
 imageButtons.forEach(btns => btns.addEventListener('click', cpuRandomPick))
 
@@ -16,11 +19,18 @@ function cpuRandomPick(e){
 }
 
 function compareChoices(nameTag, cpuSelection){
+    let results = document.querySelector('.results-column')
+    let player = document.getElementById('p1')
+    let cpu = document.getElementById('cpu')
+    let tieScore = document.getElementById('tie')
     if(nameTag === 'rock' && cpuSelection==='scissors' || nameTag==='paper' && cpuSelection==='rock' || nameTag==='scissors' && cpuSelection==='rock'){
-        console.log('player wins')
+        results.innerHTML = `Player Wins! 😁`
+        player.innerHTML = `${playerScore += 1}`
     } else if(nameTag===cpuSelection){
-        console.log('tie')
+        results.innerHTML = `Tie Game! 😑`
+        tieScore.innerHTML = `${ties += 1}`
     } else{
-        console.log('CPU Wins')
+        results.innerHTML = `CPU Wins! 😞`
+        cpu.innerHTML = `${cpuScore += 1}`
     }
 }
